cc0b5e102b0d43ea21bd4bc99c40ba8e727ae417
asker421 <asker.cisco@gmail.com>
2026-04-28 22:17:24 +0300

    Remove Android-incompatible root Java toolchain overrides

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,20 +1,4 @@
-import org.gradle.api.tasks.compile.JavaCompile
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     id("com.android.application") version "8.7.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-}
-
-subprojects {
-    tasks.withType<JavaCompile>().configureEach {
-        sourceCompatibility = JavaVersion.VERSION_17.toString()
-        targetCompatibility = JavaVersion.VERSION_17.toString()
-    }
-
-    tasks.withType<KotlinCompile>().configureEach {
-        kotlinOptions {
-            jvmTarget = "17"
-        }
-    }
 }
