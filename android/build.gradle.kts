7d72423ed615a8cf6123df71d46619fd19d36d4e
asker421 <asker.cisco@gmail.com>
2026-04-28 21:10:34 +0300

    Force JVM 17 for all Gradle subprojects

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,21 @@
+import org.gradle.api.tasks.compile.JavaCompile
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("com.android.application") version "8.7.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+}
+
+subprojects {
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = JavaVersion.VERSION_17.toString()
+        targetCompatibility = JavaVersion.VERSION_17.toString()
+        options.release.set(17)
+    }
+
+    tasks.withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
 }
