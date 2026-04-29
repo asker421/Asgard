27de096fdafb4c5c248d254621e85f82cecf937f
asker421 <asker.cisco@gmail.com>
2026-04-29 16:19:12 +0300

    Release 2.6.1 version bump

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.asgard.tv"
         minSdk = 23
         targetSdk = 35
-        versionCode = 26
-        versionName = "2.6.0"
+        versionCode = 27
+        versionName = "2.6.1"
     }
 
     buildFeatures {
