1c90a65c617a6a7f49030c9d38088276333b2799
asker421 <asker.cisco@gmail.com>
2026-04-30 17:42:37 +0300

    Fix Gradle JavaVersion syntax for 2.10.20

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion_17
+        sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
 
