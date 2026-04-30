6a1f3914d993a5645d6c3ee69f1a159183381f37
asker421 <asker.cisco@gmail.com>
2026-04-30 15:31:40 +0300

    Bump version for media task creation release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.asgard.tv"
         minSdk = 23
         targetSdk = 35
-        versionCode = 56
-        versionName = "2.10.16"
+        versionCode = 57
+        versionName = "2.10.17"
     }
 
     buildFeatures {
