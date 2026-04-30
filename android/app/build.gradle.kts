2f8b844f93a4e18443b02eeaea719cf31790a8dd
asker421 <asker.cisco@gmail.com>
2026-04-30 13:19:32 +0300

    Bump version for global states release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.asgard.tv"
         minSdk = 23
         targetSdk = 35
-        versionCode = 42
-        versionName = "2.10.2"
+        versionCode = 43
+        versionName = "2.10.3"
     }
 
     buildFeatures {
